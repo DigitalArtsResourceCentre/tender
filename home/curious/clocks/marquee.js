aa327ce973a0249c12b5d354cd254b271dec531d
Thomas <thomas.gauthier.c@gmail.com>
2022-11-22 12:02:36 -0500

    reimplemented clocks in css

--- home/curious/clocks/marquee.js
+++ home/curious/clocks/marquee.js
@@ -1,7 +1,12 @@
+const thePath = document.getElementById('thePath')
+const tp = document.getElementById('tp')
+const ellipse = document.getElementById('ellipse')
+const wrap = document.getElementById('wrap')
+
 function Init() {
   let w = wrap.clientWidth;
   let h = wrap.clientHeight;
-  ellipse.setAttributeNS(null, "viewBox", `0 0 ${w}  ${h}`);
+  ellipse.setAttribute("viewBox", `0 0 ${w}  ${h}`);
   let d = `M${w / 10},${h / 2}A${4 * w / 10},${4 * h / 10} 0 0 0 ${9 *
     w /
     10} ${5 * h / 10} A${4 * w / 10},${4 * h / 10} 0 0 0 ${w / 10} ${5 *
@@ -10,39 +15,22 @@ function Init() {
     w /
     10},${4 * h / 10} 0 0 0 ${w / 10} ${5 * h / 10}`;
 
-  thePath.setAttributeNS(null, "d", d);
-  let path_length =  thePath.getTotalLength();
-  
-  
+  thePath.setAttribute("d", d);
+  let path_length = thePath.getTotalLength();
+
+
   //begin at a bigger size than needed
   let font_size = 100;
-  ellipse.style.fontSize = font_size+"px"; 
-  
+  ellipse.style.fontSize = font_size + "px";
+
   // while the text length is bigger than half path length 
-  while(tp.getComputedTextLength() > path_length / 2 ){
+  while (tp.getComputedTextLength() > path_length / 2) {
     //reduce the font size
-    font_size -=.25;
+    font_size -= .25;
     //reset the font size 
-    ellipse.style.fontSize = font_size+"px";
+    ellipse.style.fontSize = font_size + "px";
   }
 }
 
-
-
-window.setTimeout(function() {
-  Init();
-  window.addEventListener("resize", Init, false);
-}, 15);
-
-let so = 0;
-
-function Marquee() {
-  requestAnimationFrame(Marquee);
-  tp.setAttributeNS(null, "startOffset", so + "%");
-  if (so >= 50) {
-    so = 0;
-  }
-  so += 0.02;
-}
-
-Marquee();
+Init();
+window.addEventListener("resize", Init, false);
--- home/curious/clocks/play.js
+++ home/curious/clocks/play.js
@@ -1,16 +1,128 @@
-var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
+const clocksContainer = document.getElementById('clocksContainer')
 
-let rotationSpeed = 0.09;
-let mousePos = vec2.fromValues(0, 0);
 
-let hoveredClock = null;
+function stringToEl(string) {
+  var parser = new DOMParser(),
+    content = 'text/html',
+    DOM = parser.parseFromString(string, content);
 
-let clockPositions = []
-let sqrdRadius = 0;
+  return DOM.body.childNodes[0];
+}
 
-var step = 0;
-var colorIndex = -1;
+
+
+const clock = `<div class="clockContainer">
+<svg class="clock" width="45.945709mm" height="45.945709mm" version="1.1" viewBox="0 0 45.945709 45.945709"
+xmlns="http://www.w3.org/2000/svg">
+<circle cx="22.972853" cy="22.972855" r="22.380854" stroke-width="0" fill="transparent" />
+<circle cx="22.972853" cy="22.972855" r="22.380854" stroke-width="1.18400002" />
+<g class="slowDown" style="--slowDownSpeed : 0.65s">
+<path class="clockHand" style="--revolutionTime: 1" d="m8.04279 32.800247 14.93021-9.82754"
+  stroke-linecap="round" stroke-linejoin="round" stroke-width="1.46500015" />
+  </g>
+  <g class="slowDown" style="--slowDownSpeed : 8s">
+<path class="clockHand" style="--revolutionTime: 15" d="m30.57962 14.940867-7.60662 8.03184"
+  stroke-linecap="round" stroke-linejoin="round" stroke-width="1.46500015" />
+  </g>
+</svg>
+</div>`
+
+
+const clockElem = document.importNode(stringToEl(clock), true);
+
+
+
+let width;
+let height;
+
+let vmin;
+
+let dpr;
+
+let cellSize;
+let cellSpacing;
+let cellSizeWithSpacing;
+
+let margin;
+
+let cols;
+let rows;
+
+const clocks = []
+
+function setDimensionVars() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  vmin = Math.min(width, height) / 100;
+
+  dpr = window.devicePixelRatio;
+
+  if (dpr > 1) {
+    width *= dpr;
+    height *= dpr;
+    vmin *= dpr;
+  }
+
+  cellSize = vmin * 18;
+  cellSpacing = cellSize * 0.3;
+  cellSizeWithSpacing = cellSize + cellSpacing;
+
+  margin = vmin;
+
+  cols = Math.floor((width - margin * 2) / cellSizeWithSpacing);
+  rows = Math.floor((height - margin * 2) / cellSizeWithSpacing);
+
+  clocksContainer.style.setProperty('--padding', `${margin}px`)
+
+  clocks.forEach(clock => {
+    clock.style.width = `${cellSize}px`;
+    clock.style.height = `${cellSize}px`;
+
+    clock.style['margin-left'] = `${cellSpacing / 2}px`;
+    clock.style['margin-right'] = `${cellSpacing / 2}px`;
+  })
+}
+
+setDimensionVars();
+
+window.addEventListener('resize', () => {
+  setDimensionVars();
+
+
+  const newAmount = cols * rows;
+
+  if (newAmount > clocks.length) {
+    while (clocks.length < newAmount) {
+      addClock();
+    }
+  } else if (newAmount < clocks.length) {
+    while (newAmount < clocks.length) {
+      clocks.pop().remove();
+    }
+  }
+
+})
+
+function addClock() {
+  const clock = clockElem.cloneNode(true);
+  clock.querySelector('.clock').style.setProperty('--timeOffset', `-${Math.random() * 10}s`)
+  clocks.push(clock);
+
+  clock.style.width = `${cellSize}px`;
+  clock.style.height = `${cellSize}px`;
+
+  clock.style['margin-left'] = `${cellSpacing / 2}px`;
+  clock.style['margin-right'] = `${cellSpacing / 2}px`;
+  clocksContainer.appendChild(clock)
+}
+
+for (let i = 0; i < cols * rows; i++) {
+  addClock();
+}
+
+
+var colorIndex = 0;
 var colors = [
   ['#5f0c56', '#964ec9'],
   ['#101e62', '#7675e7'],
@@ -23,201 +135,12 @@ var colors = [
   ['#277da1', '#1790d0']
 ];
 
-document.ontouchstart = color;
-document.onmousedown = color;
 
-
-function color() {
-  // console.log(rotationSpeed);
-  // rotationSpeed = rotationSpeed * 2;
-  // console.log(rotationSpeed);
+clocksContainer.addEventListener('click', () => {
   colorIndex = (++colorIndex) % colors.length;
-  canvas.style.backgroundColor = colors[colorIndex][0];
-  // boom();
-}
+  document.body.style.background = colors[colorIndex][0];
 
-function isHoveredClock(x, y) {
-  return hoveredClock && hoveredClock.cell.x == x && hoveredClock.cell.y == y
-}
+  clocksContainer.style.setProperty('--clockStrokeColor', colors[colorIndex][1])
+})
 
 
-function throttled(fn) {
-  let didRequest = false;
-  return param => {
-    if (!didRequest) {
-      window.requestAnimationFrame(() => {
-        fn(param);
-        didRequest = false;
-      });
-      didRequest = true;
-    }
-  };
-}
-
-var width, height;
-
-var vmin;
-
-var dpr;
-
-// Cell size + spacing
-var cellSize;
-var cellSpacing;
-var cellSizeWithSpacing;
-
-// Clock hands
-var longHandLength;
-var shortHandLength;
-var thickness;
-
-// Margin around the screen edge
-var margin;
-
-var cols, rows;
-
-var ox, oy;
-
-
-function setDimensionsVar() {
-  clockPositions = [];
-
-  width = window.innerWidth;
-  height = window.innerHeight;
-
-  vmin = Math.min(width, height) / 100;
-
-  dpr = window.devicePixelRatio;
-  if (dpr > 1) {
-    width *= dpr;
-    height *= dpr;
-    vmin *= dpr;
-  }
-
-  canvas.width = width;
-  canvas.height = height;
-
-  // Cell size + spacing
-  cellSize = vmin * 18;
-  cellSpacing = cellSize * 0.3;
-  cellSizeWithSpacing = cellSize + cellSpacing;
-
-
-  sqrdRadius = Math.pow(cellSize / 2, 2)
-
-  console.log("sqrdRadius", Math.sqrt(sqrdRadius));
-
-  // Clock hands
-  longHandLength = cellSize * 0.4;
-  shortHandLength = cellSize * 0.25;
-  thickness = Math.round(longHandLength / 18);
-
-  // Margin around the screen edge
-  margin = vmin;
-
-  cols = Math.floor((width - margin * 2) / cellSizeWithSpacing);
-  rows = Math.floor((height - margin * 2) / cellSizeWithSpacing);
-
-  ox = (width - (cols * cellSizeWithSpacing) + cellSpacing) / 2;
-  oy = (height - (rows * cellSizeWithSpacing) + cellSpacing) / 2;
-}
-
-setDimensionsVar();
-window.addEventListener('resize', setDimensionsVar)
-
-
-function boom() {
-
-  context.lineJoin = 'round';
-  context.lineCap = 'round';
-  context.lineWidth = thickness;
-  context.strokeStyle = colors[colorIndex][1];
-  context.clearRect(0, 0, width, height);
-
-  step += 1;
-
-  for (var x = 0; x < cols; x++) {
-    for (var y = 0; y < rows; y++) {
-
-      var px = ox + x * cellSizeWithSpacing,
-        py = oy + y * cellSizeWithSpacing;
-
-      var dx = px - width / 2;
-      var dy = py - height / 2;
-      var distanceFromCenter = Math.sqrt(dx * dx + dy * dy) / Math.min(width, height);
-
-      var rotationOffset = distanceFromCenter * 4;
-      // var rotationSpeed = 0.009;
-
-      // Move to cell
-      context.save();
-      context.translate(px + cellSize / 2, py + cellSize / 2);
-
-      if (clockPositions.length < (cols * rows)) {
-        let clockPos = vec2.fromValues(px + cellSize / 2, py + cellSize / 2);
-        clockPositions.push({ screenPos: clockPos, cell: { x, y } })
-      }
-
-      const hovered = isHoveredClock(x, y);
-
-      const clockRotationSpeed = hovered ? rotationSpeed / 20 : rotationSpeed;
-
-      // Outline
-      context.beginPath();
-      context.arc(0, 0, cellSize / 2, 0, Math.PI * 2);
-      if (hovered) {
-        context.strokeStyle = 'pink'
-      }
-      context.stroke();
-
-      // Long hand rotation
-      context.save();
-      context.rotate(rotationOffset + step * clockRotationSpeed);
-
-      // Draw from the tip of the long hand into the center
-      context.beginPath();
-      context.moveTo(longHandLength, 0);
-      context.lineTo(0, 0);
-      context.restore();
-
-      // Short hand rotation
-      context.save();
-
-
-      context.rotate(rotationOffset + step * (clockRotationSpeed / 12));
-
-      // Draw from the center to the tip of the short hand
-      context.lineTo(shortHandLength, 0);
-      context.stroke();
-
-      context.restore();
-      context.restore();
-
-    }
-  }
-
-  requestAnimationFrame(boom);
-}
-
-function handleMouseMove(e) {
-  vec2.set(mousePos, e.clientX, e.clientY);
-
-  let hasOne = false;
-
-  for (let i = 0; i < clockPositions.length; i++) {
-    const dist = vec2.sqrDist(mousePos, clockPositions[i].screenPos);
-
-    if (dist < sqrdRadius) {
-      hoveredClock = clockPositions[i];
-      hasOne = true;
-      break;
-    }
-  }
-
-  if (!hasOne) {
-    hoveredClock = null;
-  }
-}
-window.addEventListener('mousemove', throttled(handleMouseMove));
-
-color();
-boom();
